cb026b22d57a8683557cb314a6cc272d58c6a264
Lion Sherry <lsherry@va-j5.kzn.21-school.ru>
2022-07-20 13:31:53 +0300

    addded status printing

--- philo/src/philo_thread.c
+++ philo/src/philo_thread.c
@@ -1,24 +1,58 @@
 #include "../philo.h"
+void	printing(t_philo *philo, char *philo_status, char *color)
+{
+	pthread_mutex_lock(&philo->params->stdout_mutex);
+	printf("|%lld ms| %d | %sis %s%s\n", get_time() - philo->params->startup,
+		   (int ) philo->id, color, philo_status, "\033[0m");
+	pthread_mutex_unlock(&philo->params->stdout_mutex);
+}
 
+void	philo_thinking(t_philo *philo)
+{
+	printing(philo, "thinking", "\033[1;103m");
+}
 
+void	philo_sleeping(t_philo *philo)
+{
+	printing(philo, "sleeping", "\033[1;107m");
+	usleep(philo->params->time_to_sleep * 1000);
+}
+
+void	philo_eating(t_philo *philo)
+{
+	printing(philo, "eating", "\033[1;102m");
+	usleep(philo->params->time_to_eat * 1000);
+}
+
+void	philo_wait(t_philo *philo)
+{
+	(void) philo;
+}
+
+int	check_death(t_philo *philo)
+{
+	(void) philo;
+	return (1);
+}
+
+void	wait_at_start(t_philo *philo)
+{
+//	if (philo->id %)
+	(void) philo;
+}
 
 void	*philo_thread(void *vargp)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)vargp;
-//	pthread_mutex_lock(&philo->params->stdout_mutex);
-//	printf("ms: %lld\n", get_time() - philo->params->startup);
-//	sleep(1);
-//	pthread_mutex_unlock(&philo->params->stdout_mutex);
 	wait_at_start(philo);
 	while (check_death(philo))
 	{
-		philo_wait();
-		philo_eating();
-		philo_sleeping();
-		philo_thinking();
+		philo_wait(philo);
+		philo_eating(philo);
+		philo_sleeping(philo);
+		philo_thinking(philo);
 	}
-//	(void) vargp;
 	return (NULL);
 }
